a0c17c0a9eace9258af64f3136b5f0513e262481
yaellanir <yaellanir@gmail.com>
2022-12-21 14:35:06 +0200

    added usefetch and routes from end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,29 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import useFetch from "./hooks/useFetch";
+import HomePage from "./Pages/HomePage/HomePage.jsx";
+import NavBar from "./Components/NavBar/NavBar.jsx";
+import BookDisplay from "./Pages/BookDisplay/BookDisplay.jsx";
+import Error from "./Pages/Error/Error.jsx";
+
 const App = () => {
-  return <h1>Yalla balagan!!!!</h1>;
+  const [data, error] = useFetch("");
+
+  return (
+    <h1>
+      <NavBar />
+      <Routes>
+        <Route data={data} exact path="/" element={<HomePage />} />
+        <Route
+          data={data}
+          exact
+          path="/BookDisplay"
+          element={<BookDisplay />}
+        />
+        <Route exact path="*" element={<Error />} />
+      </Routes>
+    </h1>
+  );
 };
 
 export default App;
